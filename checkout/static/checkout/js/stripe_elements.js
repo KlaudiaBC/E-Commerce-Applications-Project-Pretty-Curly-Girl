01ea03468e1c594b3db475c51f64dc142a97d220
KlaudiaBC <klablaco@gmail.com>
2022-11-17 23:25:11 +0000

    PCG-41-Add checkout form data caching

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,73 +6,117 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+var client_secret = $("#id_client_secret").text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
-    base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
+  base: {
+    color: "#000",
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: "antialiased",
+    fontSize: "16px",
+    "::placeholder": {
+      color: "#aab7c4",
     },
-    invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
-    }
+  },
+  invalid: {
+    color: "#dc3545",
+    iconColor: "#dc3545",
+  },
 };
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
-
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
 
 // Handle realtime validation errors on the card element
-card.addEventListener('change', function (event) {
-    var errorDiv = document.getElementById('card-errors');
-    if (event.error) {
-        var html = `
+card.addEventListener("change", function (event) {
+  var errorDiv = document.getElementById("card-errors");
+  if (event.error) {
+    var html = `
             <span  role="alert">${event.error.message}</span>
         `;
-        $(errorDiv).html(html);
-    } else {
-        errorDiv.textContent = '';
-    }
+    $(errorDiv).html(html);
+  } else {
+    errorDiv.textContent = "";
+  }
 });
-
 
 // toggle spinner
 
-
-
 // Handle form submit
-var form = document.getElementById('payment-form');
+var form = document.getElementById("payment-form");
 
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    card.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
-    stripe.confirmCardPayment(client_secret, {
-        payment_method: {
+form.addEventListener("submit", function (ev) {
+  ev.preventDefault();
+  card.update({ disabled: true });
+  $("#submit-button").attr("disabled", true);
+  $("#payment-form").fadeToggle(100);
+  $("#loading-overlay").fadeToggle(100);
+
+  var saveInfo = Boolean($("#id-save-info").attr("checked"));
+  // From using {% csrf_token %} in the form
+  var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+  var postData = {
+    csrfmiddlewaretoken: csrfToken,
+    client_secret: client_secret,
+    save_info: saveInfo,
+  };
+  var url = "/checkout/cache_checkout_data/";
+
+  $.post(url, postData)
+    .done(function () {
+      stripe
+        .confirmCardPayment(client_secret, {
+          payment_method: {
             card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
+            billing_details: {
+              first_name: $.trim(form.first_name.value),
+              last_name: $.trim(form.last_name.value),
+              phone: $.trim(form.phone.value),
+              email: $.trim(form.email.value),
+              address: {
+                line1: $.trim(form.address_line_1.value),
+                line2: $.trim(form.address_line_2.value),
+                city: $.trim(form.city.value),
+                country: $.trim(form.country.value),
+              },
+            },
+          },
+          shipping: {
+            first_name: $.trim(form.first_name.value),
+            last_name: $.trim(form.last_name.value),
+            phone: $.trim(form.phone.value),
+            address: {
+              line1: $.trim(form.address_line_1.value),
+              line2: $.trim(form.address_line_2.value),
+              city: $.trim(form.city.value),
+              country: $.trim(form.country.value),
+              postal_code: $.trim(form.postcode.value),
+            },
+          },
+        })
+        .then(function (result) {
+          if (result.error) {
+            var errorDiv = document.getElementById("card-errors");
             var html = `
-                <span role="alert">${result.error.message}</span>`;
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            // Add webhook or plugin which will listen for the
-            // payment_intend.succeed event that handles any
-            // business critical post-payments actions
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+            $("#payment-form").fadeToggle(100);
+            $("#loading-overlay").fadeToggle(100);
+            card.update({ disabled: false });
+            $("#submit-button").attr("disabled", false);
+          } else {
+            if (result.paymentIntent.status === "succeeded") {
+              form.submit();
             }
-        }
+          }
+        });
+    })
+    .fail(function () {
+      // just reload the page, the error will be in django messages
+      location.reload();
     });
 });
