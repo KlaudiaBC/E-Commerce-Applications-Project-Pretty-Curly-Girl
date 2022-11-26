7971fdfa18b8f257d3018d3f8dc7a6b2b5e8fdd6
KlaudiaBC <klablaco@gmail.com>
2022-11-26 08:29:00 +0000

    PCG-34-Collect static and deploy to heroku

--- products/migrations/0004_product_sale.py
+++ products/migrations/0004_product_sale.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name='product',
+            model_name='Product',
             name='sale',
             field=models.BooleanField(blank=True, default=False, null=True),
         ),
