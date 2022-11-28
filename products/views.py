a6fd9ff6d42b33057965c8f8c59f529b99f2edca
KlaudiaBC <klablaco@gmail.com>
2022-11-28 03:05:21 +0000

    PCG-10-Remove staticfiles and add images

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category, MyRating
+from .models import Product, Category
 
 
 def all_products(request):
