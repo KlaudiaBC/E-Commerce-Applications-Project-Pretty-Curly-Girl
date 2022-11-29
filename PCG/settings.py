73a08c372d3c3dff92aecf81b6aaf8a38cfa89cc
KlaudiaBC <klablaco@gmail.com>
2022-11-29 06:24:33 +0000

    Add google verification tag for allauth

--- PCG/settings.py
+++ PCG/settings.py
@@ -262,6 +262,12 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'pretty-cutly-girl'
     AWS_S3_REGION_NAME = 'eu-west-3'
