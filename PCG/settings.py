012d3d5e4efe8278e6e1c96535ee6c509999d041
KlaudiaBC <klablaco@gmail.com>
2022-12-09 23:43:28 +0000

    Fix typo in settings

--- PCG/settings.py
+++ PCG/settings.py
@@ -235,7 +235,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder',)
-django_heroku.settigns(locals(), staticfiles=False)
+django_heroku.settings(locals(), staticfiles=False)
 
 # AWS S8 Bucket Config
 if 'USE_AWS' in os.environ:
