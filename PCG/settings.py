1cc68bcca2aa99503a7d2981a29bfe85964b8c49
KlaudiaBC <klablaco@gmail.com>
2022-12-13 10:46:57 +0000

    Fix templates and add sitemap.xml file

--- PCG/settings.py
+++ PCG/settings.py
@@ -37,8 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # 'DEVELOPMENT' in os.environ
-DEBUG = True
-
+DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), 'localhost']
 
