d0aef18a58399314caa9e9208551b53f1ecb141b
KlaudiaBC <klablaco@gmail.com>
2022-12-16 13:15:15 +0000

    Fix wish-list template and buttons

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from products.models import Product
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from allauth.account.signals import user_signed_up
 
 
 class UserProfile(models.Model):
@@ -16,6 +17,22 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    @receiver(user_signed_up)
+    # Populate a user who signed up via social account
+    def populate_profile(sociallogin, user, **kwargs):
+
+        user.profile = UserProfile()
+
+        if sociallogin.account.provider == 'google':
+            user_data = user.socialaccount_set.filter(
+                provider='google')[0].extra_data
+            email = user_data['email']
+            user = user_data['name']
+
+        user.userprofile.email = email
+        user.userprofile.name = user
+        user.profile.save()
 
 
 @receiver(post_save, sender=User)
